1e89a261a705f162cb3c988dbbfcc3ab9b850bee
qff <nexthop@live.cn>
2017-11-20 14:26:02 +0800

    html

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,25 +1,21 @@
 from flask import Flask, render_template, request
-from flask.ext.script import Manager
 
 
 app = Flask(__name__)
-manager = Manager(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello world</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    user_agent = request.headers.get('User-Agent')
-    return '<h1>hello, %s!</h1> <p>your browser is %s</p>' %(name, user_agent)
+    return render_template('user.html', name=name)
 
 
 
 if __name__ == '__main__':
-#    app.run(debug = True)
-    manager.run()
+    app.run(debug = True)
 
 
