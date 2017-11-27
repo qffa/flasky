31a94d052b0e5fd1255ff33678a0da3c9ca45b55
qffa <nexthop@live.cn>
2017-11-27 13:08:45 +0800

    form init

--- flasky/hello.py
+++ flasky/hello.py
@@ -2,16 +2,32 @@ from flask import Flask, render_template, request
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+app.config['SECRET_KEY'] = 'Flasky-key-1'
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('what is your name?', validators=[Required()])
+    submit = SubmitField('submit')
+
+
+
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time = datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form = form, current_time = datetime.utcnow())
 
 
 @app.route('/user/<name>')
